3b65f3eb47aec33527485fa7b3629f85e837b403
udaypratapsingh-111 <86412941+udaypratapsingh-111@users.noreply.github.com>
2021-07-09 03:11:12 +0530

    Update About.js

--- src/component/About.js
+++ src/component/About.js
@@ -10,7 +10,7 @@ function About() {
            SEC.:5CSE02<br/>
            </font>
            <img 
-      src="https://drive.google.com/file/d/1VK0mYbC1-4wwxux_lvjJ7asmW26MtegG/view?usp=sharing"
+      src="../public/assets/images/DP.jpg"
       alt="new"
       />
     </div>
